934b5277f65ca93f413275afc727aa2d31f61465
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2022-12-12 21:20:42 +0330

    feat: add download endpoint

--- app/Navisite/Program.cs
+++ app/Navisite/Program.cs
@@ -7,11 +7,19 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
 
 app.MapGet("/Ping", () => Results.Ok("Pong"))
     .WithName("Ping")
+    .WithOpenApi();
+
+app.MapGet("/Download", (string filename, IWebHostEnvironment env) =>
+    {
+        var path = Path.Combine(env.WebRootPath, filename);
+
+        return File.Exists(path) ? Results.File(File.ReadAllBytes(path)) : Results.NotFound();
+    })
+    .WithName("Download")
     .WithOpenApi();
 
 app.Run();
